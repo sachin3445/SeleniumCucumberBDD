92d6a4665024d6c575a298104bc23bf3d7df9b09
Sachin <sachin.mehetre3445@gmail.com>
2025-06-19 22:31:34 +0530

    Daily push

--- src/test/java/cucumberOptions/TestNgTestRunner.java
+++ src/test/java/cucumberOptions/TestNgTestRunner.java
@@ -1,24 +1,27 @@
 package cucumberOptions;
 
+import org.testng.annotations.DataProvider;
+
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-
+//Report types: html, json, Junit, extent
 @CucumberOptions
 		(features="./src/test/java/features",
 			glue="stepDefinitions",
 			
 			monochrome=true,
-			//tags="@AddToCart",
-			plugin={"pretty", "html:target/cucumber.html"}	
-		)
+			tags="@AddToCart or @SearchProduct",
+			plugin={"pretty", "html:target/cucumber.html", "json:target/cucumber.json",
+					"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+					"rerun:target/Failed_Test.txt"})
 
 public class TestNgTestRunner extends AbstractTestNGCucumberTests 
 {
-/*	@Override
+	@Override
 	@DataProvider(parallel=true)
 	public Object[][] scenarios()
 	{
 		return super.scenarios();
 	}
- */
+ 
 }
--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -1,8 +1,16 @@
 package stepDefinitions;
 
+import java.io.File;
 import java.io.IOException;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
 import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
+import io.cucumber.java.Scenario;
 import utilities.TestContextSearch;
 
 public class Hooks 
@@ -18,5 +26,17 @@ public class Hooks
 	public void AfterSenarios() throws IOException
 	{
 		testContextSearch.testBase.WebDriverManage().quit();
+	}
+	
+	@AfterStep
+	public void AddScreenShots(Scenario scenario) throws IOException
+	{
+		WebDriver driver = testContextSearch.testBase.WebDriverManage();
+		if(scenario.isFailed())
+		{
+		File sourceFile	=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		byte [] filecontent=FileUtils.readFileToByteArray(sourceFile);
+		scenario.attach(filecontent, "image/png", "image");
+		}
 	}
 }
